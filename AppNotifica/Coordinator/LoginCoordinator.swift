672675f3fefce5aa97f963330ad79d5b41e20971
Bernardo Brito <105974403+Bernardo-Brito@users.noreply.github.com>
2022-11-08 11:27:56 -0300

    08-11
    Criacaodateladelogin

---
+++ AppNotifica/Coordinator/LoginCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  loginCoordinator.swift
+//  AppNotifica
+//
+//  Created by IFB BIOTIC 21 on 08/11/22.
+//
+
+import Foundation
+import UIKit
+class LoginCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController){
+        self.navigationController = navigationController
+    }
+    
+    func start(){
+        
+        let viewController = LoginViewController()
+        self.navigationController.pushViewController(viewController, animated:true)
+        
+    }
+    
+}
+
